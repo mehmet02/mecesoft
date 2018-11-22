e02e9fc54219d2203d617f9e3b6cdb4ca7b60c2e
Mehmet ELÇİÇİÇEKEKİNCİ <ekinci30@hotmail.com>
2018-11-22 05:05:43 +0300

    Add files via upload

---
+++ check.php
@@ -0,0 +1,13 @@
+﻿<?
+$versioncode = "4.0"; //Güncel version kodumuz. Eğer güncelleme yapacaksak bu değeri yükseltmemiz gerek. Zaten csharp projemizdeki versionkodu 1 olduğu için güncelleme başlayacaktır.
+if($_GET["v"])
+{
+ $currentversion = $_GET["v"];
+ if($versioncode > $currentversion){
+    echo "UPDATE"; //Dönen veri update ise güncelleme başlayacak.
+  }
+   else{
+    echo "OK"; //Dönen veri OK ise güncelleme başlamayacak.
+   }
+}
+?>
